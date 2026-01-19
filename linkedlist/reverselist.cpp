3ed9ca161406f9313db3cb7d06478a8ca1b9bf27
Eman Iqbal <emaniqbal056@gmail.com>
2026-01-19 04:30:24 -0800

    Create reverselist.cpp

---
+++ linkedlist/reverselist.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    
+    Node(int value){
+        data=value;
+        next=nullptr;
+    }
+};
+
+class linkedlist{
+    public:
+    Node* head;
+    linkedlist(){
+        head=nullptr;
+    }
+    void insertatback(int value){
+        Node* newnode=new Node(value);
+        if(head==nullptr){
+            head=newnode;
+            return;
+        }
+       Node* temp=head;
+       while(temp->next!=nullptr){
+           temp=temp->next;
+       }
+       temp->next=newnode;
+    }
+  
+   
+  void popBack() {
+    if(head == nullptr) return;      // List empty
+
+    if(head->next == nullptr) {      // Only 1 node
+        delete head;
+        head = nullptr;
+        return;
+    }
+
+    Node* temp = head;
+
+    // Move temp to second-last node
+    while(temp->next->next != nullptr) {
+        temp = temp->next;
+    }
+
+    // Now temp->next is last node
+    delete temp->next;                // Delete last node
+    temp->next = nullptr;             // Second last node now becomes last
+}
+
+   void reverselist(){
+       Node* reverse=nullptr;
+       Node* current=head;
+       while(current!=nullptr){
+           Node* nextnode=current->next;
+           current->next=reverse;
+           reverse=current;
+           current=nextnode;
+       }
+       head=reverse;
+       
+   }
+    
+    void display(){
+        Node* temp=head;
+        while(temp!=nullptr){
+            cout<<temp->data<<"-> ";
+            temp=temp->next;
+        }
+        cout<<"null"<<endl;
+    }
+    
+    
+};
+int main() {
+    // Write C++ code here
+   cout << "linked list"<<endl;
+   cout<<"insertion at back"<<endl;
+   linkedlist l;
+   l.insertatback(10);
+   l.insertatback(20);
+    l.insertatback(30);
+     l.insertatback(40);
+    l.popBack();
+    l.display();
+    
+    cout<<"list after reverse"<<endl;
+    l.reverselist();
+       l.display();
+    
+    return 0;
+}
