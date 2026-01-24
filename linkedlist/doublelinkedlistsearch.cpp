68636e23994df22a9d03402e629231b6477bcfb4
Eman Iqbal <emaniqbal056@gmail.com>
2026-01-24 08:33:51 -0800

    Add doubly linked list implementation in C++
    Implemented a doubly linked list with insertion, search, and display functionalities.

---
+++ linkedlist/doublelinkedlistsearch.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    Node* next;
+    Node* prev;
+    int data;
+
+    Node(int val){
+        data = val;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+class doublelinkedlist{
+public:
+    Node* head = nullptr;
+    Node* tail = nullptr;
+
+    void insertAtBack(int val){
+        Node* newnode = new Node(val);
+
+        if(head == nullptr){
+            head = tail = newnode;
+            return;
+        }
+
+        tail->next = newnode;     // old tail ka next = new node
+        newnode->prev = tail;     // new node ka prev = old tail
+        tail = newnode;           // update tail
+    }
+    
+    void search(int x){
+        Node*temp=head;
+        while(temp!=nullptr){
+            if(temp->data==x){
+                cout<<"Value found"<<endl;
+                return;
+            }
+            temp=temp->next;
+        }
+        cout<<"Not found"<<endl;
+    }
+
+    void display(){
+        Node* temp = head;
+        while(temp != nullptr){
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "null" << endl;
+    }
+};
+
+int main() {
+    doublelinkedlist dl;
+
+    dl.insertAtBack(10);
+    dl.insertAtBack(20);
+    dl.insertAtBack(30);
+    dl.insertAtBack(40);
+
+    cout << "Doubly Linked List: " << endl;
+    dl.display();   // 10 -> 20 -> 30 -> null
+    dl.search(40);
+    
+
+    return 0;
+}
